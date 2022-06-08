da86bc93068761bd5df7ac58ad73104f02fc6466
Ahmad Alobaid <ahmad88me@gmail.com>
2022-06-08 16:26:03 +0200

    fix output path

--- scripts/fetch_images_webvowl.py
+++ scripts/fetch_images_webvowl.py
@@ -29,7 +29,7 @@ def gather_images(jar_path, opath, template_path):
             # print("DEBUG: webvowl exists: %s" % webvowl_path)
             os.rmdir(webvowl_path)
         shutil.copytree(template_path, webvowl_path)
-        comm = "java -jar %s -file %s -output %s" % (jar_path, opath, os.path.join(res_path, "data", oname+".json"))
+        comm = "java -jar %s -file %s -output %s" % (jar_path, opath, os.path.join(webvowl_path, "data", oname+".json"))
         print(comm)
         os.system(comm)
 
